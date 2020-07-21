3de5edca2180becb29a273b683562289571b4da4
juliocarneiro <juliocarneiro@gmail.com>
2020-07-21 20:48:00 -0300

    images

--- components/PageHead/index.jsx
+++ components/PageHead/index.jsx
@@ -58,9 +58,7 @@ const PageHead = () => {
         href="/favicon-16x16.png"
       />
       <link rel="manifest" href="/manifest.json" />
-      <meta name="msapplication-TileColor" content="#ffffff" />
-      <meta name="msapplication-TileImage" content="/ms-icon-144x144.png" />
-      <meta name="theme-color" content="#ffffff" />
+      <link rel="manifest" href="/manifest.json" />
       <meta name="viewport" content="initial-scale=1.0, width=device-width" />
       <title>Júlio Carneiro - Web Developer</title>
     </Head>
