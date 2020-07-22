47f9cb6caff113115875676e654679b33dba7ac0
juliocarneiro <juliocarneiro@gmail.com>
2020-07-21 23:36:33 -0300

    seo and pwa

--- components/Repositories/index.jsx
+++ components/Repositories/index.jsx
@@ -15,6 +15,16 @@ const Repositories = ({ repositories }) => {
         .map((repository) => {
           return <RepositoryCard key={repository.id} repository={repository} />;
         })}
+      <div className="md:m-12 sm:mx-8 sm:mb-8 xs:mb-8">
+        <a
+          className="text-lg hover:text-green-lighter"
+          href="https://www.github.com/juliocarneiro"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Ver mais...
+        </a>
+      </div>
     </div>
   );
 };
