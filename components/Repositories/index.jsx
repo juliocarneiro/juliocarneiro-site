40ede907152e0d61eab21a0d5494f88ab3b3ab81
juliocarneiro <juliocarneiro@gmail.com>
2020-07-21 23:18:40 -0300

    seo and pwa

--- components/Repositories/index.jsx
+++ components/Repositories/index.jsx
@@ -3,7 +3,6 @@ import React from "react";
 import RepositoryCard from "./RepositoryCard";
 
 const Repositories = ({ repositories }) => {
-  console.log(repositories);
   return (
     <div className="p-4 xm:p-12 md:grid xl:grid-cols-3 md:grid-cols-2 bg-gray1">
       <div className="md:m-4 sm:mx-8 sm:mb-8 xs:mb-8">
