65e1bd6d662fd520e24e08aeee62b9f55b8a1614
juliocarneiro <juliocarneiro@gmail.com>
2021-06-29 13:04:55 -0300

    onesignal

--- pages/_document.jsx
+++ pages/_document.jsx
@@ -10,7 +10,22 @@ export default class MyDocument extends Document {
   render() {
     return (
       <Html lang="pt-BR">
-        <Head />
+        <Head>
+          <script
+            src="https://cdn.onesignal.com/sdks/OneSignalSDK.js"
+            async=""
+          ></script>
+          <script>
+            window.OneSignal = window.OneSignal || []; OneSignal.push(function(){" "}
+            {OneSignal.init({
+              appId: "edfa24bf-bcfa-4aca-8843-7321d9e5f96c",
+              notifyButton: {
+                enable: true,
+              },
+            })}
+            )
+          </script>
+        </Head>
         <body>
           <Main />
           <NextScript />
