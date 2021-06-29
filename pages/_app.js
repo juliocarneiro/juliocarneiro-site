3a0fca8216175148be3a4fb7a6402734999fe91c
juliocarneiro <juliocarneiro@gmail.com>
2021-06-29 13:09:35 -0300

    onesignal

--- pages/_app.js
+++ pages/_app.js
@@ -7,6 +7,20 @@ const MyApp = ({ Component, pageProps }) => {
       <style jsx global>{`
         @import url("https://fonts.googleapis.com/css2?family=Ubuntu&display=swap");
       `}</style>
+      <script
+        src="https://cdn.onesignal.com/sdks/OneSignalSDK.js"
+        async=""
+      ></script>
+      <script>
+        window.OneSignal = window.OneSignal || []; OneSignal.push(function(){" "}
+        {OneSignal.init({
+          appId: "edfa24bf-bcfa-4aca-8843-7321d9e5f96c",
+          notifyButton: {
+            enable: true,
+          },
+        })}
+        )
+      </script>
       <Component {...pageProps} />
     </React.Fragment>
   );
